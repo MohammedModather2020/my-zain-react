51e0a22e1877958ab2393770a235649ed503fd1d
MohammedModather2020 <mohammedmodather144@gmail.com>
2023-10-03 17:53:40 +0300

    Fix isuues as protected route like shop , user admin

--- src/auth/AdminRoute.jsx
+++ src/auth/AdminRoute.jsx
@@ -2,8 +2,8 @@ import { Navigate } from 'react-router';
 import PropTypes from 'prop-types';
 import Layout from '../components/layout/Layout';
 
-const AdminRoute = ({ isLogin, role }) => {
-  return isLogin && role === 'admin' ? (
+const AdminRoute = ({ isLogin, roles }) => {
+  return isLogin && roles.includes('Admin') ? (
     <Layout />
   ) : (
     <Navigate to='/dashboard' />
@@ -12,7 +12,7 @@ const AdminRoute = ({ isLogin, role }) => {
 
 AdminRoute.propTypes = {
   isLogin: PropTypes.bool.isRequired,
-  role: PropTypes.string.isRequired,
+  roles: PropTypes.array.isRequired,
 };
 
 export default AdminRoute;
--- src/auth/ShopRoute.jsx
+++ src/auth/ShopRoute.jsx
@@ -2,13 +2,17 @@ import { Navigate } from 'react-router';
 import PropTypes from 'prop-types';
 import Layout from '../components/layout/Layout';
 
-const ShopRoute = ({ isLogin, role }) => {
-  return isLogin && role === 'shop' ? <Layout /> : <Navigate to='/dashboard' />;
+const ShopRoute = ({ isLogin, roles }) => {
+  return isLogin && roles.includes('Shop') ? (
+    <Layout />
+  ) : (
+    <Navigate to='/dashboard' />
+  );
 };
 
 ShopRoute.propTypes = {
   isLogin: PropTypes.bool.isRequired,
-  role: PropTypes.string.isRequired,
+  roles: PropTypes.array.isRequired,
 };
 
 export default ShopRoute;
