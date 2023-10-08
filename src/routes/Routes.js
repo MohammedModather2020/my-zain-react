faf5af2a0047ad192748575ec4ea0bc9281c14d3
MohammedModather2020 <mohammedmodather144@gmail.com>
2023-10-08 16:17:47 +0300

    Finsihed craete page pakages for showing all pakages

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -4,5 +4,14 @@ import AddCategory from '../pages/category/Add';
 import Categories from '../pages/category/Categories';
 import UpdateCategory from '../pages/category/Update';
 import Statistics from '../pages/dashboard/Statistics';
+import Packages from '../pages/package/Packages';
 
-export { Login, Register, Statistics, Categories, AddCategory, UpdateCategory };
+export {
+  Login,
+  Register,
+  Statistics,
+  Categories,
+  AddCategory,
+  UpdateCategory,
+  Packages,
+};
