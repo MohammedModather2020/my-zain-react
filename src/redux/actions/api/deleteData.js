4b73cc1c533e1c40c08fbd8cade51fea4d6e624a
MohammedModather2020 <mohammedmodather144@gmail.com>
2023-10-08 15:30:10 +0300

    Create redux method to delete any data to pass id as query url

--- src/redux/actions/api/deleteData.js
+++ src/redux/actions/api/deleteData.js
@@ -1,34 +1,38 @@
 import { toast } from 'react-toastify';
-import ApiConfig from '../../../api/ApiConfig';
 import {
+  DELETE_DATA,
   LOADED_ERROR,
-  LOADED_SUCCESS,
   PRE_REQUEST,
   REMOVE_COOKIE,
-} from '../../types';
+} from '../../type';
+import ApiConfig from '../../../api/ApiConfig';
 
-export const editData =
-  (url, token, data, navigate, redirect, type = false) =>
+export const deleteData =
+  (url, token, setShowModalConfirm, setIsShowLoading, isShowLoading) =>
   async (dispatch) => {
     try {
       dispatch({
         type: PRE_REQUEST,
       });
-      const res = await ApiConfig({
-        data: data,
-        method: type ? 'PUT' : 'PATCH',
-        url: `${url}`,
+      const res = await ApiConfig.delete(`${url}`, {
         headers: {
           authorization: `Bearer ${token}`,
         },
       });
       if (res.status === 200) {
-        toast.success(res.data.message);
-        dispatch({
-          type: LOADED_SUCCESS,
-          payload: '',
-        });
-        navigate(redirect);
+        if (res.data.status) {
+          toast.success(res.data.message);
+          dispatch({
+            type: DELETE_DATA,
+          });
+          setShowModalConfirm(false);
+          setIsShowLoading(!isShowLoading);
+        } else {
+          dispatch({
+            type: LOADED_ERROR,
+            payload: res?.data?.message,
+          });
+        }
       }
     } catch (err) {
       if (err?.response?.status === 401) {
