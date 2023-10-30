4932019b8304ff579e6474b16f46c2ae9a2647c8
MohammedModather2020 <mohammedmodather144@gmail.com>
2023-10-30 16:16:25 +0300

    Create page to update offer using multi steps compoents

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import ApiConfig from '../api/ApiConfig';
 
-export default function useData(token, url) {
+export default function  useData(token, url) {
   const [data, setData] = useState([]);
   useEffect(() => {
     ApiConfig.get(`${url}`, {
